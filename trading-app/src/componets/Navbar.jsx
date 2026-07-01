27902d4f8a19453ec611ee06f603c8019c31345d
j7226815989c-cloud <j7226815989c@gmail.com>
2026-07-01 08:08:23 -0600

    vicente navbar

---
+++ trading-app/src/componets/Navbar.jsx
@@ -0,0 +1,25 @@
+function Navbar() {
+
+    return (
+
+        <nav className="navbar navbar-dark bg-dark">
+
+            <div className="container-fluid">
+
+                <span className="navbar-brand">
+
+                    <i className="bi bi-bar-chart-fill me-2"></i>
+
+                    Trading App
+
+                </span>
+
+            </div>
+
+        </nav>
+
+    );
+
+}
+
+export default Navbar;
